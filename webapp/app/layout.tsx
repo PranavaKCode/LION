0aab93f31ffcadd18b5fafe347d8f7fb0a6d8e64
PranavaKCode <PranavaKCode@gmail.com>
2026-03-14 19:25:44 -0400

    Draft neubrutalism LION homepage

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -1,22 +1,5 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Mono, Manrope, Space_Grotesk } from "next/font/google";
 import "./globals.css";
-
-const headingFont = Space_Grotesk({
-  variable: "--font-heading-lion",
-  subsets: ["latin"],
-});
-
-const bodyFont = Manrope({
-  variable: "--font-body-lion",
-  subsets: ["latin"],
-});
-
-const monoFont = IBM_Plex_Mono({
-  variable: "--font-mono-lion",
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-});
 
 export const metadata: Metadata = {
   title: "L.I.O.N. | Lionfish Detection Control Surface",
@@ -31,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${headingFont.variable} ${bodyFont.variable} ${monoFont.variable}`}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
